a335f8f0f556fb341faf0cd4993a4a1a9851cbc3
Mayank Maurya <mayank.maurya@proqio.com>
2025-03-22 18:25:46 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,64 @@
 #![allow(unused_imports)]
 use std::rc::Rc;
-use std::{env, net::TcpListener};
-use std::io::{self, BufRead, BufReader, Read, Write};
+use std::{env};
+use std::io::{self, BufRead, Read, Write};
+use tokio::net::TcpListener;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    let stdin = io::stdin();
-    // let mut input;
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    loop {
+        let (mut socket, _) = listener.accept().await.unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-            loop {
-                let mut reader = BufReader::new(&stream);
-                let mut input: String = String::new();
+        tokio::spawn(async move {
+            loop { 
+                let mut reader = BufReader::new(&mut socket);
+                let mut input = String::new();
+                input.clear();
 
-                match reader.read_line(&mut input) {
+                match reader.read_line(&mut input).await {
                     Ok(0) => {
                         break;
                     },
                     Ok(_) => {
-                        stream.write(b"+PONG\r\n");
+                        socket.write(b"+PONG\r\n");
                     },
                     Err(e) =>  {
                         println!("{}", e);
                     }
                 }
-            }
 
-                // let _err = stream.write(b"+PONG\r\n");
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
+
+    // for stream in listener.incoming() {
+    //     match stream {
+    //         Ok(mut stream) => {
+    //         loop {
+    //             let mut reader = BufReader::new(&stream);
+    //             let mut input: String = String::new();
+
+    //             match reader.read_line(&mut input) {
+    //                 Ok(0) => {
+    //                     break;
+    //                 },
+    //                 Ok(_) => {
+    //                     stream.write(b"+PONG\r\n");
+    //                 },
+    //                 Err(e) =>  {
+    //                     println!("{}", e);
+    //                 }
+    //             }
+    //         }
+
+    //             // let _err = stream.write(b"+PONG\r\n");
+    //         }
+    //         Err(e) => {
+    //             println!("error: {}", e);
+    //         }
+    //     }
+    // }
 }

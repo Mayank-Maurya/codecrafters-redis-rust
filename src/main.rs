e824ba7644b6732e91e17be3e949d2a8f74c7ebe
Mayank Maurya <mayank.maurya@proqio.com>
2025-03-24 21:12:39 +0530

    redis protocol parser in progress

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,119 @@
 #![allow(unused_imports)]
 use std::rc::Rc;
 use std::io::{self, BufRead, Read, Write};
+use bytes::{Bytes, BytesMut};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
+use tokio_util::codec::{Decoder, Encoder};
+use memchr::memchr;
+
+#[derive(Debug)]
+struct BufSplit(usize, usize);
+
+#[derive(Debug)]
+pub enum RESPTypes {
+    String(BufSplit),
+    Error(BufSplit),
+    Int(i64),
+    Array(Vec<RESPTypes>),
+    NullArray,
+    NullBulkString,
+}
+
+#[derive(Debug)]
+pub enum RESPError {
+    UnexpectedEnd,
+    UnknownStartingByte,
+    IOError(std::io::Error),
+    IntParseFailure,
+    BadBulkStringSize(i64),
+    BadArraySize(i64),
+}
+
+impl From<std::io::Error> for RESPError {
+    fn from(e: std::io::Error) -> RESPError {
+        RESPError::IOError(e)
+    }
+}
+
+type RedisResult = Result<Option<(usize, RESPTypes)>, RESPError>;
+
+fn word(buf: &BytesMut, pos: usize) -> Option<(usize, BufSplit)> {
+    if buf.len() <= pos {
+        return None;
+    }
+
+    memchr(b'\r', &buf[pos..]).and_then(|end| {
+        if end + 1 < buf.len() {
+            Some((pos + end + 2, BufSplit(pos, pos + end)))
+        } else {
+            None
+        }
+    })
+}
+
+#[allow(clippy::unnecessary_wraps)]
+fn simple_string(buf: &BytesMut, pos: usize) -> RedisResult {
+    Ok(word(buf, pos).map(|(pos, word)| (pos, RESPTypes::String(word))))
+}
+
+fn parse(buf: &BytesMut, pos: usize) -> Result<Option<(usize, RESPTypes)>, RESPError> {
+    if buf.is_empty() {
+        return Ok(None);
+    }
+
+    match buf[pos] {
+        b'+' => simple_string(buf, pos + 1),
+        // b'-' => error(buf, pos + 1),
+        // b'$' => bulk_string(buf, pos + 1),
+        // b':' => resp_int(buf, pos + 1),
+        // b'*' => array(buf, pos + 1),
+        _ => Err(RESPError::UnknownStartingByte),
+    }
+
+}
+
+pub struct RespParser;
+
+impl Decoder for RespParser {
+    type Item = RESPTypes;
+    type Error = RESPError;
+    fn decode(&mut self, buf: &mut bytes::BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+
+        if buf.is_empty() {
+            return Ok(None);
+        }
+
+        match parse(buf,0)? {
+            Some((pos,value)) => {
+                println!("{:?}",value);
+                Ok(None)
+            },
+            None => Ok(None),
+        }
+    }
+}
+
+impl Encoder<RESPTypes> for RespParser {
+    type Error = io::Error;
+
+    fn encode(&mut self, item: RESPTypes, dst: &mut BytesMut) -> io::Result<()> {
+        write_resp_output(item, dst);
+        Ok(())
+    }
+}
+
+fn write_resp_output(item: RESPTypes, dst: &mut BytesMut) {
+    match item {
+        RESPTypes::String(s) => {
+            // TODO
+            dst.extend_from_slice(b"+");
+            // dst.extend_from_slice(&s);
+            dst.extend_from_slice(b"\r\n");
+        },
+        _ => todo!()
+    }
+}
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -11,10 +122,10 @@ async fn main() -> io::Result<()> {
     loop {
         let (mut socket, _) = listener.accept().await?;
         tokio::spawn(async move {
-            let mut buf = [0; 1024];
+            let mut buf: BytesMut = BytesMut::new();
     
             loop {
-                let n = match socket.read(&mut buf).await {
+                match socket.read(&mut buf).await {
                     Ok(0) => {
                         return
                     },
@@ -24,40 +135,15 @@ async fn main() -> io::Result<()> {
                         return;
                     }
                 };
+                
+                // todo parsing here decode and process then encode and write the data to socket
+                todo!();
     
-                if let Err(e) = socket.write_all(b"+PONG\r\n").await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
+                // if let Err(e) = socket.write_all(b"+PONG\r\n").await {
+                //     eprintln!("failed to write to socket; err = {:?}", e);
+                //     return;
+                // }
             }
         });
     }
-
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(mut stream) => {
-    //         loop {
-    //             let mut reader = BufReader::new(&stream);
-    //             let mut input: String = String::new();
-
-    //             match reader.read_line(&mut input) {
-    //                 Ok(0) => {
-    //                     break;
-    //                 },
-    //                 Ok(_) => {
-    //                     stream.write(b"+PONG\r\n");
-    //                 },
-    //                 Err(e) =>  {
-    //                     println!("{}", e);
-    //                 }
-    //             }
-    //         }
-
-    //             // let _err = stream.write(b"+PONG\r\n");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
 }

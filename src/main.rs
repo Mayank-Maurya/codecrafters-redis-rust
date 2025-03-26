c990271f4008bb5ab090b76c7f8f30645f2d9329
Mayank Maurya <mayank.maurya@proqio.com>
2025-03-26 22:44:02 +0530

    parser added

--- src/main.rs
+++ src/main.rs
@@ -37,46 +37,7 @@ impl From<std::io::Error> for RESPError {
     }
 }
 
-#[derive(Debug)]
-pub enum RedisValueRef {
-    String(Bytes),
-    Error(Bytes),
-    Int(i64),
-    Array(Vec<RedisValueRef>),
-    NullArray,
-    NullBulkString,
-    ErrorMsg(Vec<u8>),
-}
-
 type RedisResult = Result<Option<(usize, RESPTypes)>, RESPError>;
-
-// impl RESPTypes {
-//     fn get_value(self, buf: &Bytes) -> RedisValueRef {
-//         match self {
-//             RESPTypes::String(bfs) => RedisValueRef::String( buf.slice(bfs.0..bfs.1)),
-//             _ => RedisValueRef::ErrorMsg([0].to_vec()),
-//         }
-//     }
-// }
-
-// impl Encoder<RedisValueRef> for RespParser {
-//     type Error = io::Error;
-//     fn encode(&mut self, item: RedisValueRef, dst: &mut BytesMut) -> io::Result<()> {
-//         write_resp_output(item, dst);
-//         Ok(())
-//     }
-// }
-
-fn write_resp_output(item: RedisValueRef, dst: &mut BytesMut) {
-    match item {
-        RedisValueRef::String(s) => {
-            dst.extend_from_slice(b"+");
-            dst.extend_from_slice(&s);
-            dst.extend_from_slice(b"\r\n");
-        },
-        _ => println!(" not done yet")
-    }
-}
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -98,26 +59,14 @@ async fn main() -> io::Result<()> {
                                 println!("nothing came");
                             },
                             Ok(n) => {
-                                // don't convert it to string it wont work and make work miserable
-                                // convert buffer to message string
-                                // let received: std::borrow::Cow<'_, str> = String::from_utf8_lossy(&buf[..n]);
-                                // println!("Received: {}", received);
-
                                 // parse the string and get the result
                                 if let Some(result) = parse_and_decode(&buf[..n]) {
-                                    // println!("{:?}", String::from_utf8_lossy(&result));
-                                    println!("Parsed Result: {:?}", result);
                                     if let Err(e) = stream.write_all(&result).await {
                                         eprintln!("Failed to write to stream: {}", e);
                                     }
                                 } else {
                                     println!("Failed to parse the message.");
                                 }
-
-                                // write the result to stream
-                                // if let Err(e) = stream.write_all(&buf[..n]).await {
-                                //     eprintln!("Failed to write to stream: {}", e);
-                                // }
                             },
                             Err(e) => {
                                 eprintln!("failed to read from stream; err = {:?}", e);
@@ -141,8 +90,6 @@ fn parse_and_decode(buf: &[u8]) -> Option<Vec<u8>> {
 
     match parse(buf,0) {
         Ok(Some((pos, value))) => {
-            // Replace with actual logic to encode the value
-            // return Some(&buf[pos..]); // Example: returning a slice of the buffer
             return Some(encode(buf, value));
         },
         _ => return None,
@@ -184,7 +131,7 @@ fn array(buf: &[u8], mut pos: usize) -> RedisResult {
     let size_string = word(buf, pos).unwrap();
     let mut array_size = String::from_utf8_lossy(&buf[size_string.1.0..size_string.1.1]).into_owned().to_string().parse::<u8>().unwrap();
     println!("array size is: {}",array_size);
-    pos = pos + array_size as usize;
+    pos = pos + size_string.0;
     array_size = array_size * 2;
     let mut results: Vec<String> = Vec::new();
     for i in 0..array_size {
@@ -209,11 +156,11 @@ fn array(buf: &[u8], mut pos: usize) -> RedisResult {
     Err(RESPError::IntParseFailure)
 }
 
-
 fn encode(buf: &[u8], value: RESPTypes) -> Vec<u8> {
     match value {
         RESPTypes::Array(v) => {
             let mut ans: Vec<u8> = Vec::new();
+            println!("{}",v[0]);
             match v[0].as_str() {
                 "ECHO" => {
                     let length = v[1].len(); // Get the length of the string
@@ -232,11 +179,6 @@ fn encode(buf: &[u8], value: RESPTypes) -> Vec<u8> {
                 }
                 _ => todo!(),
             }
-            // if v[0] == "ECHO" {
-                
-            // } else {
-            //     return Vec::new();
-            // }
         },
         _ => todo!(),
     }
